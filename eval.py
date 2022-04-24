76a3a870a6361bde538071e5ca90935f442536da
FrankVerhoef <fsa.verhoef@chello.nl>
2022-04-24 11:18:49 +0200

    remove conversion to numpy

--- eval.py
+++ eval.py
@@ -65,7 +65,7 @@ def batcher(params, batch):
     word_embeddings = params.embedding(words_padded).type(torch.float)
     sent_repr = sent_encoder(word_embeddings, sent_lens)
 
-    return sent_repr.numpy()
+    return sent_repr
 
 
 # Set params for SentEval
